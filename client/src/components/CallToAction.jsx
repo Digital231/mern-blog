6dbee19ca1b35f6e897e41b45e232dc2114af663
Sarunas Cironka <cironka.s@gmail.com>
2024-01-24 23:24:55 +0200

    fixed anker tags

--- client/src/components/CallToAction.jsx
+++ client/src/components/CallToAction.jsx
@@ -1,6 +1,10 @@
 import { Button } from "flowbite-react";
 
 export default function CallToAction() {
+  const handleClick = () => {
+    window.open("https://www.sarunas.top/", "_blank", "noopener,noreferrer");
+  };
+
   return (
     <div className="flex flex-col sm:flex-row p-3 border border-teal-500 justify-center items-center rounded-tl-3xl rounded-br-3xl text-center">
       <div className="flex-1 justify-center flex flex-col">
@@ -9,20 +13,18 @@ export default function CallToAction() {
           Checkout this instagram like app built with react and appwrite!
         </p>
         <Button
+          onClick={handleClick}
           gradientDuoTone="purpleToPink"
           className="rounded-tl-xl rounded-bl-none"
         >
-          <a
-            href="https://www.sarunas.top/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Tapsnoklis
-          </a>
+          Tapsnoklis
         </Button>
       </div>
       <div className="p-7 flex-1">
-        <img src="../assets/images/side-img3.png" />
+        <img
+          src="https://i.postimg.cc/Xvbt9JxY/side-img3.png"
+          alt="Project Image"
+        />
       </div>
     </div>
   );
